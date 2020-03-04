06fbc3bebd03d2345191cd0e283086bae2df9fb6
godstanis <godstanis@gmail.com>
2020-03-05 02:03:22 +0300

    Optimize texture scaling

--- pkg/engine/texture.go
+++ pkg/engine/texture.go
@@ -1,7 +1,9 @@
 // Texture mapping related stuff
 package engine
 
-import "math"
+import (
+	"math"
+)
 
 // Texture sets for tile values
 var TileTextures = map[int][][]string{
@@ -64,36 +66,23 @@ var TileTextures = map[int][][]string{
 // 	For example if our texture is only 5 symbols high but we want to render it on screen with 10 height we just get the relation
 // 	in this case is 5/10=0.5 of our screen row per texture row (i.e. 1 texture row for each screen row)
 func scaleTileTextureCol(tile, h int, tileP float64) (scaled []string) {
-	c := translateToTextureCol(tile, tileP)
-	var textureCol, scaledTexture []string
-	for _, val := range TileTextures[tile] {
-		textureCol = append(textureCol, val[c])
-	}
-
-	// No need for scaling if required height is equal to texture's original height
-	if len(textureCol)-1 == h-1 {
-		return textureCol
-	}
+	c:= int(math.Round(float64(len(TileTextures[tile][0])-1) * tileP))
+	textureHeight := len(TileTextures[tile])
+	scaledTexture := make([]string, h+1)
 
 	// Actual scaling
-	perTile := float64(len(textureCol)-1) / float64(h-1)
+	perTile := float64(textureHeight-1) / float64(h-1)
 	curTileS := 0.0
 	for i := 0; i < h; i++ {
-		scaledTexture = append(scaledTexture, TileTextures[tile][int(math.Round(curTileS))][c])
+		scaledTexture[i] = TileTextures[tile][int(math.Round(curTileS))][c]
 		curTileS += perTile
 	}
 
 	// Keep first and last pixel of original texture, just to make it consistent and pleasing to look at larger distances
-	if len(scaledTexture) > 0 && len(textureCol) > 0 {
-		scaledTexture[0] = textureCol[0]
-		scaledTexture[len(scaledTexture)-1] = textureCol[len(textureCol)-1]
+	if len(scaledTexture) > 0 && textureHeight > 0 {
+		scaledTexture[0] = TileTextures[tile][0][c]
+		scaledTexture[len(scaledTexture)-1] = TileTextures[tile][textureHeight-1][c]
 	}
 
 	return scaledTexture
-}
-
-// Translates percentages to concrete texture column
-func translateToTextureCol(tile int, tileP float64) int {
-	textureWidth := len(TileTextures[tile][0])
-	return int(math.Round(float64(textureWidth-1) * tileP))
 }
